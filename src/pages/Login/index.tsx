14820978150f9f6cca6024d5747c69ee293feca0
Josimar Dos Santos Gomes <josimar.s.gomes@kroton.com.br>
2020-10-17 22:20:13 -0300

    adding handle login function and adjust InputText component

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { Input, message } from 'antd';
 
 import { Button } from 'components/Button';
 import { Container } from 'components/Container';
@@ -8,6 +9,27 @@ import InputText from 'components/InputText';
 import * as s from './styles';
 
 export default function Login() {
+  const nameInputRef = useRef<Input>(null);
+  const passwordInputRef = useRef<Input>(null);
+
+  function handleSubmit() {
+    if (!nameInputRef || !passwordInputRef) {
+      return;
+    }
+
+    const nameValue = nameInputRef.current?.input?.value || '';
+    const passwordValue = passwordInputRef.current?.input?.value || '';
+
+    if (!nameValue || !passwordValue) {
+      return message.warning('Preencha os campos corretamente');
+    }
+
+    console.log('Chama api passando', {
+      name: nameValue,
+      password: passwordValue
+    });
+  }
+
   return (
     <s.DashboardContainer>
       <Container>
@@ -15,10 +37,18 @@ export default function Login() {
           <Heading>LOGIN</Heading>
           <s.Logo />
           <s.InputGroupWrapper>
-            <InputText placeholder="name" prefix={<s.UserIcon />} />
-            <InputText placeholder="password" prefix={<s.PasswordIcon />} />
+            <InputText
+              ref={nameInputRef}
+              placeholder="name"
+              prefix={<s.UserIcon />}
+            />
+            <InputText
+              ref={passwordInputRef}
+              placeholder="password"
+              prefix={<s.PasswordIcon />}
+            />
           </s.InputGroupWrapper>
-          <Button>teste</Button>
+          <Button onClick={handleSubmit}>teste</Button>
         </s.Content>
       </Container>
     </s.DashboardContainer>
