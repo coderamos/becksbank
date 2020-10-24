156b355565583262b8225a74944b37dfd69da4c7
Diego Ramos <diego.coderamos@gmail.com>
2020-10-24 18:15:44 -0300

    fix header layout

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { LogoutOutlined } from '@ant-design/icons';
 
 import { Container } from 'components/Container';
-import Balance from 'components/Balance';
 
 import * as s from './styles';
 
@@ -12,23 +11,20 @@ type HeaderProps = {
   logout: () => void;
 };
 
-const Header: React.FC<HeaderProps> = ({ userName, balance, logout }) => {
+const Header: React.FC<HeaderProps> = ({ userName, logout }) => {
   return (
-    <s.HeaderContainer>
-      <Container>
+    <Container>
+      <s.HeaderContainer>
         <s.Logo />
-        <s.UserContainer>
-          <s.UserContent>
-            <s.UserName>Olá, {userName}</s.UserName>
-            <Balance>{balance}</Balance>
-            <s.Divisor />
-            <s.LogoutButton onClick={logout}>
-              Sair <LogoutOutlined />
-            </s.LogoutButton>
-          </s.UserContent>
-        </s.UserContainer>
-      </Container>
-    </s.HeaderContainer>
+        <s.UserInfoWrapper>
+          <s.UserName>Olá, {userName}</s.UserName>
+          <s.Divisor />
+          <s.LogoutButton onClick={logout}>
+            <LogoutOutlined />
+          </s.LogoutButton>
+        </s.UserInfoWrapper>
+      </s.HeaderContainer>
+    </Container>
   );
 };
 
