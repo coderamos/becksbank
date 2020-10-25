487a8079aa4fc331bd08316b52e8561764490082
JosimarGomes <josimar.s.gomes@kroton.com.br>
2020-10-25 20:07:12 -0300

    adjust dashboard

--- src/components/PaymentCard/styles.ts
+++ src/components/PaymentCard/styles.ts
@@ -1,5 +1,28 @@
 import styled, { css } from 'styled-components';
 
+import { Text } from 'components/Fonts';
+import barbecueImage from 'assets/images/barbecue.svg';
+
+export const NotPayments = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 40px;
+`;
+
+export const Image = styled.img.attrs({
+  src: barbecueImage
+})`
+  height: 15rem;
+`;
+
+export const Message = styled(Text)`
+  margin-top: 24px;
+  opacity: 0.6;
+  text-align: center;
+`;
+
 export const PaymentContent = styled.div`
   ${({ theme }) => css`
     margin: ${theme.spacings.medium} 0px;
